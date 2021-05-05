1ca812de1f41bed4e9f693214d196a91510e40fc
mly32 <mly32@users.noreply.github.com>
2021-05-05 00:48:31 +0000

    deploy: 1b3949ab499cd60f222ad88f268346a283538c7a

--- js/hotkey.js
+++ js/hotkey.js
@@ -1,16 +1,81 @@
-// In order for the search box to initialize properly, we have to manually click it
-document.addEventListener('DOMContentLoaded', function() {
-  const search = document.getElementById('search_input_react');
-  search.click();
-});
+// Constants
+const MAX_LEVEL = 23;
 
-// Ctrl + K should focus the search bar, emulating the Algolia search UI
 document.onkeydown = function(e) {
-  if (e.ctrlKey && e.key == 'k'){
-    const search = document.getElementById('search_input_react');
-    search.focus();
+  // Debugging
+  // console.log("Key pressed:", e.key);
 
-    // By default, using Ctrl + K in Chrome will open the location bar, so disable this
-    e.preventDefault();
+  // Do not do anything if we have any modifier keys pressed down
+  if (e.ctrlKey || e.shiftKey || e.altKey || e.metaKey) {
+    return;
   }
+
+  // Do not do anything if we have the search box focused
+  const searchInputElements = document.getElementsByClassName("DocSearch-Input");
+  for (const searchInputElement of searchInputElements) {
+    if (document.activeElement === searchInputElement) {
+      return;
+    }
+  }
+
+  if (e.key === "Enter") {
+    const buttons = document.getElementsByClassName("button--secondary button--lg");
+    if (buttons.length >= 1) {
+      buttons[0].click();
+    }
+  } else if (e.key === "ArrowLeft") {
+    const buttons = document.getElementsByClassName("pagination-nav__link");
+    if (buttons.length >= 1) {
+      buttons[0].click();
+    }
+  } else if (e.key === "ArrowRight") {
+    const buttons = document.getElementsByClassName("pagination-nav__link");
+    if (buttons.length >= 2) {
+      buttons[1].click();
+    } else if (buttons.length >= 1) {
+      buttons[0].click();
+    }
+  } else if (e.key === "l") {
+    const levelString = window.prompt("Enter the level that you want to go to:");
+    if (levelString === null || levelString === "") {
+      return;
+    }
+
+    const level = parseIntSafe(levelString);
+    if (Number.isNaN(level)) {
+      return;
+    }
+
+    if (level < 1 || level > MAX_LEVEL) {
+      return;
+    }
+
+    window.location = `/docs/level-${level}`;
+  }
+}
+
+// parseIntSafe is a more reliable version of parseInt
+// By default, "parseInt('1a')" will return "1", which is unexpected
+// This returns either an integer or NaN
+function parseIntSafe(input) {
+  // Remove all leading and trailing whitespace
+  let trimmedInput = input.trim();
+
+  const isNegativeNumber = trimmedInput.startsWith("-");
+  if (isNegativeNumber) {
+    // Remove the leading minus sign before we match the regular expression
+    trimmedInput = trimmedInput.substring(1);
+  }
+
+  if (/^\d+$/.exec(trimmedInput) === null) {
+    // "\d" matches any digit (same as "[0-9]")
+    return NaN;
+  }
+
+  if (isNegativeNumber) {
+    // Add the leading minus sign back
+    trimmedInput = `-${trimmedInput}`;
+  }
+
+  return parseInt(trimmedInput, 10);
 }
